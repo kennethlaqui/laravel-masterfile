06448e4456a81a079dd931a19aa313c3af89e2aa
kennethlaqui <kennethlaqui@yahoo.com>
2019-08-26 22:27:08 +0800

    SSS

--- public/js/custom.js
+++ public/js/custom.js
@@ -37,74 +37,97 @@ $(document).ready(function() {
     } );
 
 
-    var payr_dir = $('#payr_dir').DataTable({
-        orderCellsTop: true,
-        fixedHeader: true,
-        select:         true,
-        deferRender:    true,
-        scrollY:        200,
-        scrollX:        true,
-        scrollCollapse: true,
-        scroller:       true,
+    // var payr_dir = $('#payr_dir').DataTable({
+    //     orderCellsTop: true,
+    //     fixedHeader: true,
+    //     select:         true,
+    //     deferRender:    true,
+    //     scrollY:        200,
+    //     scrollX:        true,
+    //     scrollCollapse: true,
+    //     scroller:       true,
 
-    });
+    // });
 
-    var payr_dtl = $('#payr_dtl').DataTable({
-        orderCellsTop: true,
-        fixedHeader: true,
-        select:         true,
-        deferRender:    true,
-        scrollY:        200,
-        scrollX:        true,
-        scrollCollapse: true,
-        scroller:       true,
+    // var payr_dtl = $('#payr_dtl').DataTable({
+    //     "language": {
+    //         "emptyTable": "",
+    //         "sZeroRecords": ""
+    //       }
+    //     // orderCellsTop: true,
+    //     // fixedHeader: true,
+    //     // select:         true,
+    //     // deferRender:    true,
+    //     // scrollY:        200,
+    //     // scrollX:        true,
+    //     // scrollCollapse: true,
+    //     // scroller:       true,
 
-    });
+//     // });
+//     // $('#payr_dir tbody').on( 'click', 'tr', function ( event ) {
+//     $('#payr_dirx tbody').on( 'click', 'tr', function(event) {
+//             var appl_prd = payr_dir.row( this ).data()[0];
+//             event.preventDefault();
+//             $.ajax({
+//                 url: 'sss/{appl_prd}',
+//                 method: 'GET',
+//                 data: { appl_prd: appl_prd },
+//                 // data: { appl_prd: $('#payr_dir').serialize('appl_prd') },
+//                 dataType: "json",
+//                 success:function(data){
+//                     // console.log(data);
+//                 //    $json = JSON.parse(data)
+//                     // var stringified = JSON.stringify(data);
+//                     // var parsedObj = JSON.parse(stringified);
+//                     console.log(data);
 
-    $('#payr_dir tbody').on( 'click', 'tr', function () {
-        var cntrl_no = payr_dir.row( this ).data()[0];
-        $.ajax({
-        url: 'sss/{id}',
-        type: 'GET',
-        data: { id: cntrl_no },
-        dataType: 'json',
-        success: function(response)
-        {
-            console.log(response);
-            var event_data = '';
-            $.each( function(index, value){
-                alert( index + ": " + value );
-                // event_data += '<tr>';
-                // event_data += '<td>'+value.cntrl_no+'</td>';
-                // event_data += '<td>'+value.appl_prd+'</td>';
-                // event_data += '</tr>';
-            });
-            $("#payr_dtl").append(event_data);
-            console.log(event_data);
-        }
-    });
+//                     $('#payr_dtlx').DataTable( {
+//                         "processing": true,
+//                         "serverSide": true,
+//                         "dataSrc": "",
+//                         "ajax": data,
+//                         "columns" : [
+//                             { "data" : "cntrl_no" },
+//                             { "data" : "appl_prd" }
+//                         ]
+//                     } );
+//                     // $.each(data, function (index, value){
+//                     //     console.log(value);
+//                     //     var html = '<tr>';
+//                     //     html += '<td>'+value.cntrl_no+'</td>';
+//                     //     html += '<td>'+value.appl_prd+'</td>';
+//                     //     $('#table_data').prepend(html);
+//                     // });
+
+//                 }
+
+//             });
+//             // var url = $(this).attr('href');
+//             // $('div#container').load(url);
+
+//             // console.log(url);
+//             // Load the content from the link's href attribute
+//             // jQuery('#payr_dir').load(href);
+//             // // Avoid the link click from loading a new page
+//             // event.preventDefault();
+//             // console.log(href);
+//     //     var cntrl_no = payr_dir.row( this ).data()[0];
+//     //     console.log(cntrl_no);
+//     //     $.ajax({
+//     //     url: 'sss/{id}',
+//     //     type: 'GET',
+//     //     data: { id: cntrl_no },
+//     //     success: function(response)
+//     //     {
+
+//     //         $('#payr_dtl').html(response);
+
+//     //     }
+//     // });
+// } );
+
 } );
 
-
-    // table.buttons().container()
-    //     .appendTo( '.col-md-6:eq(0)' );
-} );
-
-
-// function checkBox(){
-
-//     var bio_reqd = document.getElementsByClassName('bio_reqd');
-
-//     if(bio_reqd[0].checked){
-//         var T = document.getElementsByClassName('bio_reqd').value = 'T';
-//         alert(T);
-//     }
-//     if(!bio_reqd[0].checked){
-//         var F = document.getElementsByClassName('bio_reqd').value = 'F';
-//         alert(F);
-//     }
-
-// }
 
 $('#birthday').datepicker({
     uiLibrary: 'bootstrap4'
